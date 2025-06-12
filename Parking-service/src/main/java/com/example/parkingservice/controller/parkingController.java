7c37ebd0b4d5d3bd987081c5e45fab05bd83a03b
ASUS <nethmidileksha6@gmail.com>
2025-06-12 10:03:20 +0530

    add derrieres and yml file

---
+++ Parking-service/src/main/java/com/example/parkingservice/controller/parkingController.java
@@ -0,0 +1,17 @@
+package com.example.parkingservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/parking")
+public class parkingController {
+
+
+    @GetMapping("all")
+    public String getParking(){
+        return "parking service";
+    }
+
+}

6c59865b584c34bce42f55d96619bfb74a31a31e
ASUS <nethmidileksha6@gmail.com>
2025-06-12 09:59:09 +0530

    add derrieres and yml file

---
+++ vehicl-service/src/test/java/com/example/vehiclservice/VehiclServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vehiclservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
